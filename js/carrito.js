028897239401541ff7f6246226ee965ae932d6b4
Jesus <jesusdavidgonzales1102@gmail.com>
2022-11-14 21:35:44 -0500

    Mejoras en las funciones

--- js/carrito.js
+++ js/carrito.js
@@ -13,16 +13,28 @@ class Carrito {
             image: producto.querySelector('img').src,
             precio: producto.querySelector('.description__card .precio').textContent
         }
+        this.TotalCompra(InfoProducto.precio)
         this.InsertarDatosProducto(InfoProducto)
+    }
+
+
+    TotalCompra(precioProducto){
+        const valor = document.querySelector('#valor_total')
+        const valorproducto = parseFloat(precioProducto)
+        const total = valor.value + valorproducto
+        valor.value = total
     }
 
     InsertarDatosProducto(datos){
         const Producto = document.createElement('tr')
         Producto.innerHTML = `
             <td>${datos.name}</td>
-            <td>${datos.precio}</td>
+            <td class='price'>${datos.precio}</td>
             <td>
                 <img src='${datos.image}' style='width:100px'>
+            </td>
+            <td>
+                <i class="fa-solid fa-trash"></i>
             </td>`
         
         listaProducto.appendChild(Producto)
